a8e8591853fd7d5bd0615baf41154a0fe8474243
helwig <helwig@susqu.edu>
2015-03-28 13:25:38 -0400

    3.28.15 laptop comments

--- app/src/main/java/com/helwigdev/a/dogecoinutilities/MainActivity.java
+++ app/src/main/java/com/helwigdev/a/dogecoinutilities/MainActivity.java
@@ -39,7 +39,7 @@ import java.util.ArrayList;
 public class MainActivity extends Activity
         implements NavigationDrawerFragment.NavigationDrawerCallbacks {
 	//TODO add caught error reporting function
-		implements NavigationDrawerFragment.NavigationDrawerCallbacks {
+
 	//declarations
 
 	protected static final String TAG = "MainActivity";
